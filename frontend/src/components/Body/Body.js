383e87422e14ed4438a16076ae17b6ea992c1174
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-12-17 10:49:58 -0500

    adding slideshow

--- frontend/src/components/Body/Body.js
+++ frontend/src/components/Body/Body.js
@@ -1,7 +1,9 @@
 import Signin from "./Signin"
+import Slideshow from "./Slideshow"
 
 export default function Body() {
     return(<div>
               <Signin />
+              <Slideshow />
            </div>)
 };
--- frontend/src/components/Body/Slideshow.js
+++ frontend/src/components/Body/Slideshow.js
@@ -1,3 +1,14 @@
+import Picture1 from '../../images/Picture1.jpg'
+
 export default function Slideshow() {
-    
+
+   const tempStyle = {
+      height: "40vh",
+      width: "100%",
+      backgroundImage: `url(${Picture1})`,
+      display: "flex",
+      alignItems: "flex-end",
+      justifyContent: "center"
+   }
+   return(<div style={tempStyle}><div className="mini-slide-show full-width">Mini Slide Show</div></div>) 
 };
