62289f58167d94e2116b653d151ee6a4bab8eba8
Bl1xvan <cflavien1@my.brookdalecc.edu>
2023-01-02 21:20:25 -0500

    adding adds

--- frontend/src/components/Body/AddListItem.js
+++ frontend/src/components/Body/AddListItem.js
@@ -1,3 +1,28 @@
+const GridComp = ({gridClass, addItem}) => {
+    const activeGrid={
+        single: "1fr",
+        doubleRows: "repeat(2, 1fr)"
+    }
+
+    const styles={
+        display: "grid",
+        gridTemplateColumns: activeGrid[gridClass]
+    }
+
+    return(    
+    <div style={styles} className="add-group-grid">
+        {addItem.map(item => {
+            const {image, title, link} = item
+            return(<div><h4>{title}</h4><p>{image}</p><p>{link}</p></div>)
+        })}
+    </div>)
+}
+
+
 export default function AddListItem({addHeader, gridClass, addItem}){
-  return(<></>)
+  return(
+  <div className="add-group">
+    <h3>{addHeader}</h3>
+    <GridComp gridClass={gridClass} addItem={addItem}/>
+  </div>)
 }
--- frontend/src/components/Body/Adds.js
+++ frontend/src/components/Body/Adds.js
@@ -3,29 +3,35 @@ import AddListItem from "./AddListItem"
 const addList = [
     {
         addHeader: "New Year deals", 
-        gridClass: 1, 
+        gridClass: "single", 
         addItems: [
         {image: "image 1", title: "title1-1", link: "link"}
      ]},
     {
         addHeader: "Keep shopping for",
-        gridClass: 1, 
+        gridClass: "single", 
         addItems: [
         {image: "image 1", title: "title2-1", link: "link"}
         ]
     },
     {
         addHeader:"The New Year Sale",
-        gridClass: 1, 
+        gridClass: "doubleRow", 
         addItems: [
-        {image: "image 1", title: "title3-1", link: "link"}
+        {image: "image 1", title: "title3-1", link: "link"},
+        {image: "image 2", title: "title3-2", link: "link"},
+        {image: "image 3", title: "title3-3", link: "link"},
+        {image: "image 4", title: "title3-4", link: "link"}
         ]
     },
     {
         addHeader: "Send eGift Cards instantly",
-        gridClass: 1, 
+        gridClass: "doubleRow", 
         addItems: [
-        {image: "image 1", title: "title4-1", link: "link"}
+        {image: "image 1", title: "title4-1", link: "link"},
+        {image: "image 2", title: "title4-2", link: "link"},
+        {image: "image 3", title: "title4-3", link: "link"},
+        {image: "image 4", title: "title4-4", link: "link"}
         ]
     }
 ]
